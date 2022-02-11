70c50f010cb3301576477b5b95fccbc6134eba37
Fillipe <ss.fillipe@gmail.com>
2022-02-11 20:06:48 -0300

    Second commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
-        'PASSWORD': os.os.environ['DB_PASSWORD'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT']
     }
